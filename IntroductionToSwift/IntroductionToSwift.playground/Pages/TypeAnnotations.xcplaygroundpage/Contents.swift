a752930592fa1f436d83f562747b3dd02638a963
Imael <96964536+kabikira@users.noreply.github.com>
2022-12-21 17:20:17 +0900

    Revert "add TypeAnnotations"

--- IntroductionToSwift/IntroductionToSwift.playground/Pages/TypeAnnotations.xcplaygroundpage/Contents.swift
+++ IntroductionToSwift/IntroductionToSwift.playground/Pages/TypeAnnotations.xcplaygroundpage/Contents.swift
@@ -1,32 +1,7 @@
-// https://www.hackingwithswift.com/quick-start/beginners/how-to-use-type-annotations
+//: [Previous](@previous)
 
-let surname: String = "Lasso"
-var score: Double = 0
+import Foundation
 
-let playerName: String = "Roy"
+var greeting = "Hello, playground"
 
-var luckyNumber: Int = 13
-let pi: Double = 3.141
-var isAuthenticated: Bool = true
-var albums: [String] = ["Red", "Fearless"]
-var user: [String: String] = ["id": "@twostraws"]
-var books: Set<String> = Set(["The Bluest Eye", "Foundation", "Girl, Woman, Other"])
-
-
-var teams: [String] = [String]()
-var cities: [String] = []
-var clues = [String]()
-
-enum UIStyle {
-    case light, dark, system
-}
-
-var style = UIStyle.light
-
-let username: String
-// lots of complex logic
-username = "@twostraws"
-// lots more complex logic
-print(username)
-
-//let score: Int = "Zero"
+//: [Next](@next)
