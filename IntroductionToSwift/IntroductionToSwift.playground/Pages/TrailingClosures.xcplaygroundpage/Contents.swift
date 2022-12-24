ea1004b6ffb608ab59f9e3933730e2bf1bb30f9f
kabikira <kabikirar2@protonmail.com>
2022-12-24 23:49:11 +0900

    add TrailingClosures

---
+++ IntroductionToSwift/IntroductionToSwift.playground/Pages/TrailingClosures.xcplaygroundpage/Contents.swift
@@ -0,0 +1,60 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let team = ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
+
+//let captainFirstTeam = team.sorted(by: { (name1: String, name2: String) -> Bool in
+//    if name1 == "Suzanne" {
+//        return true
+//    } else if name2 == "Suzanne" {
+//        return false
+//    }
+//
+//    return name1 < name2
+//})
+//
+//print(captainFirstTeam)
+
+
+//let captainFirstTeam = team.sorted { name1, name2 in
+//    if name1 == "Suzanne" {
+//        return true
+//    } else if name2 == "Suzanne" {
+//        return false
+//    }
+//
+//    return name1 < name2
+//}
+
+let captainFirstTeam = team.sorted {
+    if $0 == "Suzanne" {
+        return true
+    } else if $1 == "Suzanne" {
+        return false
+    }
+
+    return $0 < $1
+}
+
+//let reverseTeam = team.sorted {
+//    return $0 > $1
+//}
+let reverseTeam = team.sorted { $0 > $1 }
+
+
+let tOnly = team.filter { $0.hasPrefix("T") }
+print(tOnly)
+
+
+let uppercaseTeam = team.map { $0.uppercased() }
+print(uppercaseTeam)
+
+func animate(duration: Double, animations: () -> Void) {
+    print("Starting a \(duration) second animation…")
+    animations()
+}
+
+animate(duration: 3) {
+    print("Fade out the image")
+}
